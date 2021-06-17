f751e8c0641593134796f9ebc0bb80ac76f5a146
Gary Christopher Drocella Jr <gdrocell@Garys-iMac.local>
2021-06-17 15:08:42 -0400

    vbox init

---
+++ vbox-init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo curl -sL https://deb.nodesource.com/setup_14.x -o nodesource_setup.sh;
+
+sudo apt-get install emacs -y;
+sudo apt-get install nodejs -y;
+sudo apt-get install npm -y;
